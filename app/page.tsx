e295db3748f392ff18a55f1b8b0acab4b47d4e25
Assemjaheen123 <assemjaheen@gmail.com>
2025-09-22 16:08:24 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+import AssemJaheenSite from "../components/AssemJaheenSite";
+import "./globals.css";
+
+export default function Page() {
+  return <AssemJaheenSite />;
+}
