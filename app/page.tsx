04340d1e33e8db39297bcfae73a9353ddf18a75a
Assemjaheen123 <assemjaheen@gmail.com>
2025-09-24 05:59:37 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-import AssemJaheenSite from "@/components/AssemJaheenSite";
+'use client';
+import React from 'react';
+import AssemJaheenSite from '../components/AssemJaheenSite';
+import './globals.css';
+
 export default function Page() {
   return <AssemJaheenSite />;
 }
